631f3327a1ac02e73865ea3a58a5a3aae39dfa8e
Vijayrawal9171 <132963272+Devilvija@users.noreply.github.com>
2024-01-29 20:00:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://editor.analyticsvidhya.com/uploads/76889recommender-system-for-movie-recommendation.jpg");
+             background-image: url("https://i.pinimg.com/originals/52/92/ef/5292ef7dd72f9cf4a9b7001712f82bad.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
@@ -45,6 +45,7 @@ movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
 similarity = pickle.load(open('similarity (1).pkl', 'rb'))
+data = pd.read_csv('data.csv')
 
 st.title('Movie Recommender System')
 
@@ -57,26 +58,60 @@ if st.button('Recommend'):
     names, posters = recommend(Selected_movie_name)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.image(posters[0], width=200)
-        st.text("")
+        st.image(posters[0], width=250)
         st.header(names[0])
+        # Get director and cast information from the data DataFrame
+        movie_info = data[data['movie_name'] == names[0]]
+        st.markdown("---")
+        st.subheader('Director:')
+        st.write(movie_info['directors'].values[0])
+        st.subheader('Cast:')
+        st.write(movie_info['stars'].values[0])
 
     with col2:
-        st.image(posters[1], width=200)
-        st.text("")
+        st.image(posters[1], width=250)
         st.header(names[1])
+        # Get director and cast information from the data DataFrame
+        movie_info = data[data['movie_name'] == names[1]]
+        st.markdown("---")
+        st.subheader('Director:')
+        st.write(movie_info['directors'].values[0])
+        st.subheader('Cast:')
+        st.write(movie_info['stars'].values[0])
 
     with col3:
-        st.image(posters[2], width=200)
-        st.text("")
+        st.image(posters[2], width=250)
         st.header(names[2])
+        # Get director and cast information from the data DataFrame
+        movie_info = data[data['movie_name'] == names[2]]
+        st.markdown("---")
+        st.subheader('Director:')
+        st.write(movie_info['directors'].values[0])
+        st.subheader('Cast:')
+        st.write(movie_info['stars'].values[0])
 
     with col4:
-        st.image(posters[3], width=200)
-        st.text("")
+        st.image(posters[3], width=250)
         st.header(names[3])
+        # Get director and cast information from the data DataFrame
+        movie_info = data[data['movie_name'] == names[3]]
+        st.markdown("---")
+        st.subheader('Director:')
+        st.write(movie_info['directors'].values[0])
+        st.subheader('Cast:')
+        st.write(movie_info['stars'].values[0])
 
     with col5:
-        st.image(posters[4], width=200)
-        st.text("")
+        st.image(posters[4], width=250)
         st.header(names[4])
+        # Get director and cast information from the data DataFrame
+        movie_info = data[data['movie_name'] == names[4]]
+        st.markdown("---")
+        st.subheader('Director:')
+        st.write(movie_info['directors'].values[0])
+        st.subheader('Cast:')
+        st.write(movie_info['stars'].values[0])
+
+        st.markdown("---")
+        st.subheader(''':white[PROJECT DONE BY: 'Mr. Vijay Balnath Rawal']''')
+        st.markdown("---")
